e33044200e6b0573578d92d0b5cd2a727f183365
ALP300 <aitorpalomino1@gmail.com>
2025-06-05 17:03:00 -0500

    Ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,3 +5,11 @@ Created on Thu Jun  5 16:45:28 2025
 @author: aitor
 """
 
+import pandas as pd
+import numpy as np
+datos={'Nombre':['Leonardo','Darik','Julio','Alex'], 'Calificaciones':[
+    '60','70','70', '80'], 'Deportes':['Natación','Basquet', 'Fútbol', 'Fútbol'],
+    'Materias':['Química','Matemática', 'Historia', 'vida']
+       }
+df= pd.DataFrame(datos)
+print(df)
